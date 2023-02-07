5ff9d8bf4410dd763375bb2907652c15e8c2f530
rk <rebeccakruk@outlook.com>
2023-02-07 09:17:45 -0700

    created DB procedures for item table

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import Flask, request
 import json
 from dbhelpers import run_statement
+
+app = Flask(__name__)
+
+@app.get('/api/apistore/items')
+def get_items():
+    result = run_statement("CALL list_items()")
+    if (type(result) == list):
+        result_json = json.dumps(result, default=str)
+        items = json.loads(result_json)
+        return items
+    else:
+        return "Something went wrong, please try again"
+    
+app.run(debug = True)
